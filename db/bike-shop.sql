cbddb4d145f41f2b134a07786343e0d3249b630c
nopparat231 <23.noop@gmail.com>
2018-11-07 00:27:56 +0700

    up sql

--- db/bike-shop.sql
+++ db/bike-shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2018 at 05:23 PM
+-- Generation Time: Nov 06, 2018 at 06:27 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 5.6.36
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tbl_admin` (
-  `admin_id` int(11) NOT NULL,
+  `admin_id` int(6) UNSIGNED ZEROFILL NOT NULL,
   `admin_user` varchar(20) NOT NULL,
   `admin_pass` varchar(20) NOT NULL,
   `admin_name` varchar(100) NOT NULL,
@@ -45,8 +45,8 @@ CREATE TABLE `tbl_admin` (
 --
 
 INSERT INTO `tbl_admin` (`admin_id`, `admin_user`, `admin_pass`, `admin_name`, `admin_address`, `admin_tel`, `admin_email`, `status`, `date_save`) VALUES
-(1, '1', '1', 'admin', 'asd', '0', '', 'admin', '2017-08-30 01:57:41'),
-(2, '55', '2', 'ทดสอบ แอดมิน', '', '0', '', 'staff', '2018-01-25 04:13:06');
+(000001, '1', '1', 'admin', 'asd', '0', '', 'admin', '2017-08-30 01:57:41'),
+(000002, '2', '2', 'ทดสอบ แอดมิน', '', '0', '', 'staff', '2018-01-25 04:13:06');
 
 -- --------------------------------------------------------
 
@@ -80,7 +80,7 @@ INSERT INTO `tbl_bank` (`b_id`, `b_number`, `b_name`, `b_type`, `b_owner`, `bn_n
 --
 
 CREATE TABLE `tbl_member` (
-  `mem_id` int(11) NOT NULL,
+  `mem_id` int(6) UNSIGNED ZEROFILL NOT NULL,
   `mem_name` varchar(50) NOT NULL,
   `mem_address` varchar(255) NOT NULL,
   `mem_tel` varchar(10) NOT NULL,
@@ -98,9 +98,9 @@ CREATE TABLE `tbl_member` (
 --
 
 INSERT INTO `tbl_member` (`mem_id`, `mem_name`, `mem_address`, `mem_tel`, `mem_username`, `mem_password`, `mem_email`, `status`, `sid`, `active`, `dateinsert`) VALUES
-(7, 'dddd', 'dddd', '0902020202', 'dddd', 'dddd', '23.noop@gmail.com', 'user', 'u3ia1e8d7ddbmcpqt7b0uq2tr0', 'yes', '2018-08-26 03:32:16'),
-(8, 'eeee', 'eeee', '2222222222', 'eeee', 'eeee', '23.noop@gmail.com', 'user', 'kgddv7hf9n54kltfgslnjmu7t3', 'yes', '2018-08-26 03:41:35'),
-(9, 'aaaa', 'bbbb', '2121212121', 'bbb', 'bbb', '23.noop@gmail.com', 'user', '7vff77gqj3l3ilmi20il62btd2', 'yes', '2018-08-26 05:55:53');
+(000007, 'dddd', 'dddd', '0902020202', 'dddd', 'dddd', '23.noop@gmail.com', 'user', 'u3ia1e8d7ddbmcpqt7b0uq2tr0', 'yes', '2018-08-26 03:32:16'),
+(000008, 'eeee', 'eeee', '2222222222', 'eeee', 'eeee', '23.noop@gmail.com', 'user', 'kgddv7hf9n54kltfgslnjmu7t3', 'yes', '2018-08-26 03:41:35'),
+(000009, 'aaaa', 'bbbb', '2121212121', 'bbb', 'bbb', '23.noop@gmail.com', 'user', '7vff77gqj3l3ilmi20il62btd2', 'yes', '2018-08-26 05:55:53');
 
 -- --------------------------------------------------------
 
@@ -135,6 +135,15 @@ CREATE TABLE `tbl_order` (
   `order_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `tbl_order`
+--
+
+INSERT INTO `tbl_order` (`order_id`, `mem_id`, `name`, `address`, `email`, `phone`, `order_status`, `pay_slip`, `b_name`, `b_number`, `pay_date`, `pay_amount`, `postcode`, `order_date`) VALUES
+(0000000003, 7, 'dddd', 'dddd', '23.noop@gmail.com', '0902020202', 4, '44421295020181106_231732.jpg', 'กสิกรไทย', '435-545-65454-545', '2018-11-06', 19189.38, '', '2018-11-06 23:17:21'),
+(0000000004, 7, 'dddd', 'dddd', '23.noop@gmail.com', '0902020202', 2, '134863616320181107_001904.jpg', 'ไทยพาณิชย์', '235-545-66684-845', '2018-11-07', 47032.92, '', '2018-11-07 00:18:39'),
+(0000000005, 7, 'dddd', 'dddd', '23.noop@gmail.com', '0902020202', 1, '', '', '', '0000-00-00', 0.00, '', '2018-11-07 00:20:52');
+
 -- --------------------------------------------------------
 
 --
@@ -149,6 +158,18 @@ CREATE TABLE `tbl_order_detail` (
   `p_c_qty` int(11) NOT NULL,
   `total` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_order_detail`
+--
+
+INSERT INTO `tbl_order_detail` (`d_id`, `order_id`, `p_id`, `p_name`, `p_c_qty`, `total`) VALUES
+(1, 1, 27, '2018 TREK MARLIN 6', 1, '14370.1'),
+(2, 1, 29, 'SURLY LONG HAUL TRUCKER', 15, '626688.3'),
+(3, 2, 26, '2015 MERIDA BIG 9 - 500D', 1, '23516.46'),
+(4, 3, 32, 'MERIDA JULIET7-100 (2017)', 1, '19189.38'),
+(5, 4, 26, '2015 MERIDA BIG 9 - 500D', 2, '47032.92'),
+(6, 5, 32, 'MERIDA JULIET7-100 (2017)', 1, '19189.38');
 
 -- --------------------------------------------------------
 
@@ -177,14 +198,14 @@ CREATE TABLE `tbl_product` (
 --
 
 INSERT INTO `tbl_product` (`p_id`, `t_id`, `p_name`, `p_size`, `p_detial`, `p_price`, `p_unit`, `p_img1`, `p_img2`, `p_view`, `p_qty`, `p_ems`, `date_save`) VALUES
-(26, 15, '2015 MERIDA BIG 9 - 500D', '15', '<p><strong>2015 Merida BIG NINE - 500</strong></p>\r\n\r\n<p>&nbsp;<strong>FRAME</strong>&nbsp;: Aluminum Big Seven TFS</p>\r\n\r\n<p>&nbsp;<strong>FORK</strong>&nbsp;: Rock Shox (เป็นโช็คแอร์)</p>\r\n\r\n<p>&nbsp;- Shimano Deore/SLX 30 Sp</p>\r\n\r\n<p>&nbsp;- ขนาดล้อ 29 นิ้ว</p>\r\n', 21900, 'คัน', 'img132868451620180823_231954.jpg', '', 10, 3, 78, '2018-08-23 16:19:54'),
-(27, 15, '2018 TREK MARLIN 6', '18', '<p>TREK MARLIN6 -Orange- 2018</p>\r\n\r\n<p>- 24 สปีดส์</p>\r\n\r\n<p>- ดิสก์น้ำมัน</p>\r\n\r\n<p>- ไซส์ 13.5 , 15.5 (ล้อขนาด 27.5)</p>\r\n\r\n<p>- ไซส์ 18.5, 19.5, 21.5 (ล้อขนาด 27.5)</p>\r\n\r\n<p>_Ride the fastest wheel that fits with Smart Wheel Size<br />\r\n_Ride-tuned aluminum frame is XC light, mountain tough<br />\r\n_Extended size range gives you a better fit for a better ride<br />\r\n_Backed by Trek&#39;s limited lifetime warranty</p>\r\n\r\n<p>_Smart Wheel Size ให้ประสิทธิภาพการปั่นของคุณยอดเยี่ยมถึงขีดสุดได้ในทุกๆขนาดเฟรม ให้คุณไปได้เร็วกว่าในเส้นทางราบ และไต่เขาได้อย่างยอดเยี่ยมในเส้นทางเทรล<br />\r\n_เฟรมอลูมินั่มจากเทรค นอกเหนือจากเทคโนโลยีการผลิตจะยอดเยี่ยม สร้างเฟรมที่มีน้ำหนักเบา แข็งแรงทนทานแล้ว Ride-Tuned Frame ยังมีการออกแบบองศาเฟรมแต่ละขนาดอย่างเหมาะสม คงไว้ซึ่งประสิทธิภาพการปั่น<br />\r\n_ขนาดของเฟรมที่กว้างครอบคลุมตั้งแต่ 13.5&quot; ถึง 23&quot; ให้คุณสามารถเลือกใช้งานเฟรมขนาดที่เหมาะสมสำหรับคุณมากที่สุด เพื่อให้ได้ประสิทธิภาพการใช้งานที่ดีที่สุด</p>\r\n\r\n<p>&nbsp;</p>\r\n', 13400, 'คัน', 'img164019166420180823_232216.jpg', 'img264019166420180823_232216.jpg', 11, 3, 30, '2018-08-23 16:22:16'),
+(26, 15, '2015 MERIDA BIG 9 - 500D', '15', '<p><strong>2015 Merida BIG NINE - 500</strong></p>\r\n\r\n<p>&nbsp;<strong>FRAME</strong>&nbsp;: Aluminum Big Seven TFS</p>\r\n\r\n<p>&nbsp;<strong>FORK</strong>&nbsp;: Rock Shox (เป็นโช็คแอร์)</p>\r\n\r\n<p>&nbsp;- Shimano Deore/SLX 30 Sp</p>\r\n\r\n<p>&nbsp;- ขนาดล้อ 29 นิ้ว</p>\r\n', 21900, 'คัน', 'img132868451620180823_231954.jpg', '', 10, 0, 78, '2018-08-23 16:19:54'),
+(27, 15, '2018 TREK MARLIN 6', '18', '<p>TREK MARLIN6 -Orange- 2018</p>\r\n\r\n<p>- 24 สปีดส์</p>\r\n\r\n<p>- ดิสก์น้ำมัน</p>\r\n\r\n<p>- ไซส์ 13.5 , 15.5 (ล้อขนาด 27.5)</p>\r\n\r\n<p>- ไซส์ 18.5, 19.5, 21.5 (ล้อขนาด 27.5)</p>\r\n\r\n<p>_Ride the fastest wheel that fits with Smart Wheel Size<br />\r\n_Ride-tuned aluminum frame is XC light, mountain tough<br />\r\n_Extended size range gives you a better fit for a better ride<br />\r\n_Backed by Trek&#39;s limited lifetime warranty</p>\r\n\r\n<p>_Smart Wheel Size ให้ประสิทธิภาพการปั่นของคุณยอดเยี่ยมถึงขีดสุดได้ในทุกๆขนาดเฟรม ให้คุณไปได้เร็วกว่าในเส้นทางราบ และไต่เขาได้อย่างยอดเยี่ยมในเส้นทางเทรล<br />\r\n_เฟรมอลูมินั่มจากเทรค นอกเหนือจากเทคโนโลยีการผลิตจะยอดเยี่ยม สร้างเฟรมที่มีน้ำหนักเบา แข็งแรงทนทานแล้ว Ride-Tuned Frame ยังมีการออกแบบองศาเฟรมแต่ละขนาดอย่างเหมาะสม คงไว้ซึ่งประสิทธิภาพการปั่น<br />\r\n_ขนาดของเฟรมที่กว้างครอบคลุมตั้งแต่ 13.5&quot; ถึง 23&quot; ให้คุณสามารถเลือกใช้งานเฟรมขนาดที่เหมาะสมสำหรับคุณมากที่สุด เพื่อให้ได้ประสิทธิภาพการใช้งานที่ดีที่สุด</p>\r\n\r\n<p>&nbsp;</p>\r\n', 13400, 'คัน', 'img164019166420180823_232216.jpg', 'img264019166420180823_232216.jpg', 11, 2, 30, '2018-08-23 16:22:16'),
 (28, 16, '2015 RIDLEY FENIX (CARBON)', '19', '<p>RIDLEY FENIX (Carbon) - 2015<br />\r\n<br />\r\nFRAME FENIX C : 24T HM UNIDIRECTIONAL CARBON<br />\r\nFORK FENIX C : 24T HM UDV CARBON / ALLOY STEERER<br />\r\nDRIVETRAIN : SHIMANO 105 11 SPEED (Mix)<br />\r\nWHEELSET : FULCRUM RACING SPORT</p>\r\n', 44000, 'คัน', 'img123966966620180823_232354.jpg', '', 1, 10, 54, '2018-08-23 16:23:54'),
-(29, 17, 'SURLY LONG HAUL TRUCKER', '15', '<p>จักรยานทัวร์ริ่งรุ่นยอดนิยม เหมาะสำหรับการขี่ท่องเที่ยวระยะไกล&nbsp;<br />\r\nมีคุณสมบัติแข็งแรง ทนทานและมีความนุ่มนวลเพียงพอกับการขนสัมภาระหนักๆ&nbsp;<br />\r\nด้วยคุณสมบัติจักรยานทัวร์ริ่งแท้โดยมีหูยึดตะแกรงที่หางหลังและที่ตะเกียบหน้า<br />\r\nสามารถใช้เบรคผีเสือได้เพื่อจะได้ไม่เกะกะจุดยึดตะแกรงและบังโคลน</p>\r\n', 39000, 'คัน', 'img139683125820180823_232521.jpg', '', 0, 20, 46, '2018-08-23 16:25:21'),
+(29, 17, 'SURLY LONG HAUL TRUCKER', '15', '<p>จักรยานทัวร์ริ่งรุ่นยอดนิยม เหมาะสำหรับการขี่ท่องเที่ยวระยะไกล&nbsp;<br />\r\nมีคุณสมบัติแข็งแรง ทนทานและมีความนุ่มนวลเพียงพอกับการขนสัมภาระหนักๆ&nbsp;<br />\r\nด้วยคุณสมบัติจักรยานทัวร์ริ่งแท้โดยมีหูยึดตะแกรงที่หางหลังและที่ตะเกียบหน้า<br />\r\nสามารถใช้เบรคผีเสือได้เพื่อจะได้ไม่เกะกะจุดยึดตะแกรงและบังโคลน</p>\r\n', 39000, 'คัน', 'img139683125820180823_232521.jpg', '', 0, 5, 46, '2018-08-23 16:25:21'),
 (25, 15, 'BIANCHI JAB 27.3 (2017)', '17', '<p>เฟรมอลูมิเนียม Superlight ซ่อนสายลบรอยเชื่อม ขนาดล้อ 27.5 นิ้ว มากับเกียร์ Shimano XT/Deore แบบ 2x10 สปีด เบรคไฮดรอลิค ชุดจานหน้า แบบกระโหลกกลวง Hollowtech Octalink ของ Shimano พร้อมโช๊ค Rockshox มี Remote lockout</p>\r\n\r\n<p>มีมาให้เลือก 2&nbsp;สี คือ สีเขียวซีเลสเต้ (ด้าน), สีดำด้านคาดเขียว</p>\r\n\r\n<p>Model : JAB 27.3 XT/Deore 2x10 speed</p>\r\n\r\n<p>Frame : Alu 6061, Fork : Rock Shox 30 Silver Headset : FSA Orbit 1.5B ZS-1 Shifters : Shimano Deore 2x10 sp Rear derailleur : Shimano XT RD-M781 Shadow Design 10 sp Front derailleur : Shimano Deore FD-M610-B Crankset : Shimano FC-M523 38/24T Hollowtech Octalink, BB : Shimano BB-ES300 Chain : KMC X10 EPT&nbsp;<br />\r\nSprocket : Shimano Deore HG50-10sp, 11-13-15-17-19-21-24-28-32-36T Brakes : Shimano M396 Wheels : WTB XC21 TCS</p>\r\n\r\n<p>&nbsp;</p>\r\n', 32300, 'คัน', 'img188859081820180823_225652.jpg', 'img288859081820180823_225652.jpg', 5, 0, 30, '2018-08-23 15:56:52'),
 (30, 18, 'BMC ALPENCHALLENGE AC02', '20', '<p><strong>Frame :</strong>&nbsp;Alpha Gold Aluminum</p>\r\n\r\n<p><strong>Front Susp. :</strong>&nbsp;SR Suntour , Coil Spring , 63mm. travel</p>\r\n\r\n<p><strong>Drivetrain :</strong>&nbsp;Shimano Acera 3*8 SP. Compact</p>\r\n\r\n<p><strong>Wheel&#39;s size :</strong>&nbsp;700c</p>\r\n', 14000, 'คัน', 'img1178139422120180823_232808.jpg', '', 2, 62, 30, '2018-08-23 16:27:33'),
 (31, 19, 'LAMBORGHINI 20\" CONCETTO', '18', '<p>รถพับรูปลักษณ์แปลกตาสวยงาม ลิขสิทธิ์แท้จาก Tonino Lamborghini</p>\r\n\r\n<p><strong>Frame</strong>&nbsp;:Alloy</p>\r\n\r\n<p><strong>Brakes</strong>&nbsp;:Tektro R320</p>\r\n\r\n<p><strong>Wheel Size</strong>:20 inch wheels 451</p>\r\n\r\n<p><strong>Speeds</strong>:18</p>\r\n\r\n<p><strong>Weight</strong>:11.4 kg</p>\r\n\r\n<p><strong>Color</strong>&nbsp;: Gross black</p>\r\n', 20000, 'คัน', 'img112614947020180823_232934.jpg', '', 0, 18, 60, '2018-08-23 16:29:34'),
-(32, 21, 'MERIDA JULIET7-100 (2017)', '15', '<p><br />\r\n- Frame JULIET TFS<br />\r\n- Fork Suntour SR 27 XCM HLO 100<br />\r\n- ชุดขับ Shimano Alivio/Altus 27 Speed</p>\r\n', 17900, 'คัน', 'img182381907120180823_233044.jpg', '', 1, 19, 34, '2018-08-23 16:30:44');
+(32, 21, 'MERIDA JULIET7-100 (2017)', '15', '<p><br />\r\n- Frame JULIET TFS<br />\r\n- Fork Suntour SR 27 XCM HLO 100<br />\r\n- ชุดขับ Shimano Alivio/Altus 27 Speed</p>\r\n', 17900, 'คัน', 'img182381907120180823_233044.jpg', '', 1, 17, 34, '2018-08-23 16:30:44');
 
 -- --------------------------------------------------------
 
@@ -203,11 +224,11 @@ CREATE TABLE `tbl_type` (
 
 INSERT INTO `tbl_type` (`t_id`, `t_name`) VALUES
 (15, 'จักรยานเสือภูเขา'),
-(18, 'ซิตี้ไบค์/ไฮบริด/ ครุยเซอร์'),
+(18, 'ซิตี้ไบค์/ไฮบริด'),
 (17, 'จักรยานทัวร์ริ่ง'),
 (16, 'จักรยานเสือหมอบ'),
 (19, 'จักรยานพับ'),
-(20, 'จักรยานสำหรับเด็ก / BMX'),
+(20, 'จักรยานสำหรับเด็ก'),
 (21, 'จักรยานสำหรับผู้หญิง');
 
 --
@@ -264,7 +285,7 @@ ALTER TABLE `tbl_type`
 -- AUTO_INCREMENT for table `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `admin_id` int(6) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_bank`
@@ -276,19 +297,19 @@ ALTER TABLE `tbl_bank`
 -- AUTO_INCREMENT for table `tbl_member`
 --
 ALTER TABLE `tbl_member`
-  MODIFY `mem_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `mem_id` int(6) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tbl_order`
 --
 ALTER TABLE `tbl_order`
-  MODIFY `order_id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `order_id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tbl_order_detail`
 --
 ALTER TABLE `tbl_order_detail`
-  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_product`
